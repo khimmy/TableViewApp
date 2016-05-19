2c0697ed26f5e20734f8198ddb9609a1ae0504ca
F839-008 M <mf839-008@70-12-242-219.pools.spcsdns.net>
2016-05-19 15:39:37 +0900

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by MF839-008 on 5/19/16.
+//  Copyright © 2016 SamsungSDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
